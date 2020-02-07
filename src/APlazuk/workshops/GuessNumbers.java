7c3ae8439e7e9cec35934c19bc85909e9f8b8b55
Aleksandra Plazuk <aleksandraplazuk@o2.pl>
2020-02-07 16:11:05 +0100

    Gra w zgadywanie liczb

--- src/APlazuk/workshops/GuessNumbers.java
+++ src/APlazuk/workshops/GuessNumbers.java
@@ -1,4 +1,43 @@
 package APlazuk.workshops;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumbers {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int randomNumber = r.nextInt(100);
+        guessNumber(randomNumber);
+    }
+        public static void guessNumber(int randomNumber){
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Zgadnij liczbę: ");
+
+            while (!scanner.hasNextInt()) {
+                scanner.nextLine();
+                System.out.println("To nie jest liczba. Zgadnij liczbę");
+            }
+            int a = scanner.nextInt();
+
+            //sprawdzanie warunków
+            numberCheck(randomNumber, a);
+        }
+
+    private static void numberCheck(int randomNumber, int a) {
+        if (a < randomNumber){
+            System.out.println("Za mało!");
+            guessNumber(randomNumber);
+        } else if (a>randomNumber){
+            System.out.println("Za dużo!");
+            guessNumber(randomNumber);
+        } else {
+            System.out.println("Zgadłeś!");
+        }
+    }
+
+
 }
+
+
